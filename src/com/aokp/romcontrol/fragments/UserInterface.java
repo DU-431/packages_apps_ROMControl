46783df74edfb011a1fa505ab2e44e23c1eef7d3
Jubakuba <Jubakuba@gmail.com>
2013-09-28 02:16:20 -0600

    ROMControl: LengthFilter on Carrier Label
    Set's a filter on carrier label length
        Was previously possible to extend
        carrier label to two+ lines which
        only showed top half of second line
        (also cleans up "summary" so you
        can't set a huge amount of text)
    
    Change-Id: Ie96dac9f28c85170e922ae9df659d025d7071f11

--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -33,6 +33,7 @@ import android.preference.PreferenceScreen;
 import android.preference.TwoStatePreference;
 import android.provider.MediaStore;
 import android.provider.Settings;
+import android.text.InputFilter;
 import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
@@ -559,6 +560,10 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
 
             // Set an EditText mView to get user input
             final EditText input = new EditText(getActivity());
+            final InputFilter[] filter = new InputFilter[1];
+            filter[0] = new InputFilter.LengthFilter(40);
+
+            input.setFilters(filter);
             input.setText(mCustomLabelText != null ? mCustomLabelText : "");
             alert.setView(input);
             alert.setPositiveButton(getResources().getString(R.string.ok),
