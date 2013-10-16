6c1e5f4437777a149b028b56fde1ed0de1c52de6
Alex <mazdarider23@gmail.com>
2013-10-16 15:58:15 -0500

    [2/2] Added OMNI-ROM Lockscreen battery ring to RC
    Change-Id: I9402cfc3ec9ac8229eb849cf25f204074417a1b1

--- src/com/aokp/romcontrol/fragments/UserInterface.java
+++ src/com/aokp/romcontrol/fragments/UserInterface.java
@@ -120,6 +120,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     private static final String STATUS_BAR_NETWORK_COLOR = "status_bar_network_color";
     private static final String PREF_RECENTS_STYLE = "pref_recents_style";
     private static final String PREF_RECENTS_CLEAR = "pref_recents_clear";
+    private static final String BATTERY_AROUND_LOCKSCREEN_RING = "battery_around_lockscreen_ring";
 
     private static final int REQUEST_PICK_WALLPAPER = 201;
     //private static final int REQUEST_PICK_CUSTOM_ICON = 202; //unused
@@ -168,6 +169,7 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
     private ListPreference mStatusBarNetStatsUpdate;
     private CheckBoxPreference mStatusBarNetworkStats;
     private ColorPickerPreference mNetworkColor;
+    private CheckBoxPreference mLockRingBattery;
 
     private AnimationDrawable mAnimationPart1;
     private AnimationDrawable mAnimationPart2;
@@ -225,6 +227,10 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
         mShowImeSwitcher = (CheckBoxPreference) findPreference(PREF_IME_SWITCHER);
         mShowImeSwitcher.setChecked(Settings.System.getBoolean(mContentResolver,
                 Settings.System.SHOW_STATUSBAR_IME_SWITCHER, true));
+
+        mLockRingBattery = (CheckBoxPreference) findPreference(BATTERY_AROUND_LOCKSCREEN_RING);
+        mLockRingBattery.setChecked(Settings.System.getInt(mContentResolver,
+                Settings.System.BATTERY_AROUND_LOCKSCREEN_RING, 0) == 1);
 
         mStatusbarSliderPreference = (CheckBoxPreference) findPreference(PREF_STATUSBAR_BRIGHTNESS);
         mStatusbarSliderPreference.setChecked(Settings.System.getBoolean(mContentResolver,
@@ -643,6 +649,11 @@ public class UserInterface extends AOKPPreferenceFragment implements OnPreferenc
         } else if (preference == mStatusBarNetworkStats) {
             Settings.System.putInt(mContentRes,
                     Settings.System.STATUS_BAR_NETWORK_STATS,
+                    ((CheckBoxPreference)preference).isChecked() ? 1 : 0);
+            return true;
+        } else if (preference == mLockRingBattery) {
+            Settings.System.putInt(mContentRes,
+                    Settings.System.BATTERY_AROUND_LOCKSCREEN_RING,
                     ((CheckBoxPreference)preference).isChecked() ? 1 : 0);
             return true;
         } else if (preference == mRamBar) {
